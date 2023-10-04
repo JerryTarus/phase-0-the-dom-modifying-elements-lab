3bd7b3830467c8b87932a365d5559931ff11408b
Jerry Tarus <jerrytarus@gmail.com>
2023-10-04 08:22:15 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code here!
+
+
+// const names = document.createElement(names);
+
+// for (let j = 0; i < 5; i++) {
+//     const li = document.createElement("names");
+//     li.textContent = (i + 1).toString();
+//     names.append(names);
+// }
+
+// element.append(names);
+
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   ul.append(li);
+// }
+
+// element.append(ul);
+
+main.remove()
+const newHeader = document.createElement("heading");
+newHeader.id = "teaser"
+newHeader.innerHTML = "Jerry you guy my guy!!!"
+
+
+
+
